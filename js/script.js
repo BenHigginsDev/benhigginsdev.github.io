9d58888f69ecfe63c06a511cf3ff733b880cb9cc
Ben <emailbenhiggins@gmail.com>
2026-06-28 22:51:27 +0100

    Hopefully eradicated dark mode flash by executing a script at the top of the HTML

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 // Grab the button element from the HTML
 const toggleButton = document.querySelector('.theme-toggle');
 
-// Check on load if the user previously chose light mode
-if (localStorage.getItem('porfolio-theme') === 'light') {
-    document.body.classList.add('light-mode');
-}
-
 // Listen for the user to click the button
 toggleButton.addEventListener('click', () => {
-    // Flip the "light-mode" class on or off on the body tag
-    document.body.classList.toggle('light-mode');
-
-    // Check if light mode is currently active and store the preference in localStorage
-    if (document.body.classList.contains('light-mode')) {
-        localStorage.setItem('porfolio-theme', 'light'); // Save preference as light
+const isLight = document.documentElement.classList.contains('light-mode');
+    
+    if (isLight) {
+        // Switch to Dark
+        document.documentElement.classList.remove('light-mode');
+        document.documentElement.classList.add('dark-mode');
+        localStorage.setItem('portfolio-theme', 'dark');
     } else {
-        localStorage.setItem('porfolio-theme', 'dark'); // Save preference as dark
+        // Switch to Light
+        document.documentElement.classList.remove('dark-mode');
+        document.documentElement.classList.add('light-mode');
+        localStorage.setItem('portfolio-theme', 'light');
     }
 });
